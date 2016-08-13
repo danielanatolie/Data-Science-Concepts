012027a514eda6eb0d2b17d4b95a711a45902cc3
danielislam <daniel.anatolie@gmail.com>
2016-08-13 14:37:24 -0700

    Mean, median and mode analysis

--- incomeAnalysis.py
+++ incomeAnalysis.py
@@ -1,4 +1,25 @@
+#Generating 10,000 incomes ranging from $15000 to $27000
 import numpy as np
+incomes=np.random.normal(27000, 15000, 10000)
+#(center, sd, amountOfData)
+print np.mean(incomes)
 
-incomes = np.random.normal(27000, 15000, 10000)
-np.mean(incomes)
+#Ploting a histogram of the data
+import matplotlib.pyplot as plt
+#plt.hist(incomes, 50)
+#plt.show()
+
+#Median of income data
+print np.median(incomes)
+
+#How outlier effects data set
+incomes = np.append(incomes, [1000000000])
+np.median(incomes)
+#Mean is most affected
+print np.mean(incomes)
+
+#Mode Analysis
+ages = np.random.randint(18, high=90, size=500)
+# print ages
+from scipy import stats
+print stats.mode(ages)
